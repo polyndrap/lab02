01c6439cca3277b45e192e9ff30e6458c1048760
polyndrap <161497193+polyndrap@users.noreply.github.com>
2024-05-27 16:30:53 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,8 +2,8 @@
 #include <string>
 
 int main() {
-   string name;       // name 
-   std::cin >> name;    // input name
+   string name;       // name.
+   std::cin >> name;    // input name.
    std::cout << "hello from " << name << endl;  // output hello name
    return 0;
 }
